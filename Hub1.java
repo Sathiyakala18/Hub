9c811370fb6b4754a1e47d4dd96c0424d4d80bd0
Sathiyakala18 <sathiyakala.ravi@gmail.com>
2022-08-06 23:56:39 +0530

    Hub1.java

---
+++ Hub1.java
@@ -0,0 +1,18 @@
+package Github;
+
+public class Hub1 {
+	
+public static void main(String[] args) {
+		
+		int count=0;
+		for (int i=0; i<=100; i++){
+		if(i%2!=0){
+		count++;
+		}
+		}
+		System.out.println(count);
+
+		}
+		}
+
+
